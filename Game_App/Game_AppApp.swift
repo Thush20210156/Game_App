dd2c9e8282f1a46f0d041c475d7c8ded8bda865e
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-12-03 17:07:50 +0530

    Initial Commit

---
+++ Game_App/Game_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_AppApp.swift
+//  Game_App
+//
+//  Created by Thushini Abeysuriya on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct Game_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Game_AppTests/Game_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Game_AppTests.swift
+//  Game_AppTests
+//
+//  Created by Thushini Abeysuriya on 2024-12-03.
+//
+
+import Testing
+@testable import Game_App
+
+struct Game_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
